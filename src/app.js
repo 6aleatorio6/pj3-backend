227314518930a64fdf36a7da803522cd8fd74135
Leonardo <leonardolfelix12@gmail.com>
2024-03-22 17:06:55 -0300

    build: add cors e prisma como devDependencies

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import express from 'express';
+import cors from 'cors';
 import rotaPaia from './routers/rotaPaia.js';
 
 const app = express();
 
+const arrayDeOrigins = JSON.parse(process.env.CORS_ORIGINS);
+
 // middleware
 app.use(
+  cors({ origin: arrayDeOrigins }),
   morgan('dev'),
   express.json(),
   express.urlencoded({ extended: false }),
