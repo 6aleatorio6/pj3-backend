6789303bc3ace8843133de17962ceda5b8c0c5c4
= <=>
2024-03-26 14:21:24 -0300

    feat(relatorioModel): add count total visitas e funcionarios

--- src/models/relatorioModel.js
+++ src/models/relatorioModel.js
@@ -2,31 +2,85 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-const totalUsuarios = async ({sexo, cidade, foto, numeroUsuarios = 0, puleUsuarios = 0}) => {
+const totalUsuarios = async ({sexo, cidade, email, foto, numeroUsuarios, puleUsuarios = 0}) => {
 
-    const filtro = {}
-    if (sexo !== undefined) filtro.sexo = sexo;
-    if (cidade !== undefined) filtro.cidade = cidade;
-    if (foto !== undefined) filtro.foto = foto;
+    const filtro = {
+        sexo,
+        cidade
+    }
 
     const countUsers = await prisma.usuario.count({
+        select: {
+            foto: foto,
+            email: email
+        },
         where: filtro,
         skip: puleUsuarios,
         take: numeroUsuarios
     })
-    const users = await prisma.usuario.findMany()
+    const users = await prisma.usuario.findMany({
+        select: {
+            id: false,
+            senhaHash: false
+        },
+        where: filtro,
+        skip: puleUsuarios,
+        take: numeroUsuarios
+    })
     return {countUsers, users}
 }
 
-const totalFuncionarios = async () => {
-    const countUsers = await prisma.usuario.count()
-    const users = await prisma.usuario.findMany()
+const totalFuncionarios = async ({sexo, cidade, email, foto, numerofuncionario, pulefuncionario = 0}) => {
+    const filtro = {
+        sexo,
+        cidade
+    }
+
+    const countUsers = await prisma.funcionario.count({
+        select: {
+            foto: foto,
+            email: email
+        },
+        where: filtro,
+        skip: pulefuncionario,
+        take: numerofuncionario
+    })
+    const users = await prisma.funcionario.findMany({
+        select: {
+            id: false,
+            senhaHash: false
+        },
+        where: filtro,
+        skip: pulefuncionario,
+        take: numerofuncionario
+    })
     return {countUsers, users}
 }
 
-const totalVisitas = async () => {
-    const countUsers = await prisma.usuario.count()
-    const users = await prisma.usuario.findMany()
+const totalVisitas = async (numerovisitas, pulevisitas = 0) => {
+    const filtro = {
+        sexo,
+        cidade
+    }
+
+    const countUsers = await prisma.visitas.count({
+        select: {
+            foto: foto,
+            email: email
+        },
+        where: filtro,
+        skip: pulevisitas,
+        take: numerovisitas
+    })
+    const users = await prisma.visitas.findMany({
+        select: {
+            id: false,
+            senhaHash: false
+        },
+        where: filtro,
+        skip: pulevisitas,
+        take: numerovisitas
+    })
     return {countUsers, users}
 }
 
