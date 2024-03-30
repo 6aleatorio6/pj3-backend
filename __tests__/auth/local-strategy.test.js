b2ba6ec4a50c01b4ea99e43aa765a5076250f960
leonardo <leonardolfelix12@gmail.com>
2024-03-30 18:39:34 -0300

    test(auth): add tests para  as strategy local e jwt

---
+++ __tests__/auth/local-strategy.test.js
@@ -0,0 +1,95 @@
+import prisma from '../../src/prisma.js';
+import bcrypt from 'bcrypt';
+import app from '../../src/app.js';
+import supertest from 'supertest';
+import jwt from 'jsonwebtoken';
+
+const request = supertest(app);
+
+jest.spyOn(jwt, 'sign').mockImplementation((x) => x);
+
+async function reqUserPost(data, isNotUser) {
+  const rota = !isNotUser ? 'user' : 'funcionario';
+  const res = await request.post('/auth/login/' + rota).send(data);
+  // if (res.error) console.log(res.error);
+
+  return res;
+}
+
+const mockFindUser = jest.spyOn(prisma.usuario, 'findFirst');
+const mockFindFuncio = jest.spyOn(prisma.funcionario, 'findFirst');
+const userMock = {
+  id: 1,
+  email: 'usuario@example.com',
+  senhaHash: bcrypt.hashSync('senhaCerta', 10),
+};
+
+describe('local-strategy', () => {
+  afterEach(() => {
+    mockFindUser.mockReset();
+  });
+
+  it('ceu azul | email e senha estao correto, conta USER', async () => {
+    mockFindUser.mockResolvedValueOnce(userMock);
+    const response = await reqUserPost({
+      email: 'n importa',
+      senha: 'senhaCerta',
+    });
+
+    const body = {
+      message: 'sucesso no login',
+      token: {
+        id: 1,
+        roles: 'USER',
+      },
+    };
+
+    expect(response.ok).toBeTruthy();
+    expect(response.body).toEqual(body);
+  });
+
+  it('ceu azul | email e senha estao correto, conta Funcionario', async () => {
+    mockFindFuncio.mockResolvedValueOnce({ ...userMock, roles: 'ADM' });
+    const response = await reqUserPost(
+      {
+        email: 'n importa',
+        senha: 'senhaCerta',
+      },
+      true,
+    );
+
+    const body = {
+      message: 'sucesso no login',
+      token: {
+        id: 1,
+        roles: 'ADM',
+      },
+    };
+
+    expect(response.ok).toBeTruthy();
+    expect(response.body).toEqual(body);
+  });
+
+  it('céu triste | sem body ou body invalido', async () => {
+    mockFindUser.mockResolvedValueOnce(userMock);
+    const response = await reqUserPost({});
+
+    expect(response.badRequest).toBeTruthy();
+    expect(mockFindUser).toHaveBeenCalledTimes(0); // n chegou a usar o cb verify
+  });
+
+  it('céu triste | o email não existe', async () => {
+    mockFindUser.mockResolvedValueOnce({}); // simulando que o prisma n encoutrou nd
+    const response = await reqUserPost({ email: 'n importa', senha: '123' });
+
+    expect(response.unauthorized).toBeTruthy();
+    expect(mockFindUser).toHaveBeenCalledTimes(1);
+  });
+
+  it('céu triste | email correto, mas senha errada', async () => {
+    mockFindUser.mockResolvedValueOnce(userMock);
+    const response = await reqUserPost({ email: 'n importa', senha: '123' });
+
+    expect(response.unauthorized).toBeTruthy();
+  });
+});
