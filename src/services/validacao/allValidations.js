074ab8ea07ab17c77ce406400cce068d51afdb0d
leonardo <leonardolfelix12@gmail.com>
2024-05-03 17:40:26 -0300

    feat: criado função para facilitar utilizar o zod

--- src/services/validacao/allValidations.js
+++ src/services/validacao/allValidations.js
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const validations = z.object({
+export const allValid = z.object({
   id: z.number().int(),
   uuid: z.string(),
 
@@ -33,35 +33,3 @@ export const validations = z.object({
   dataDaVisita: z.string().max(45),
   dataDaDescoberta: z.string().max(45),
 });
-
-
-export const validationsMessages = {
-  id: 'ID deve ser um número inteiro.',
-  uuid: 'UUID deve ser uma string.',
-
-  googleId: 'Google ID deve ter no máximo 45 caracteres.',
-  facebookId: 'Facebook ID deve ter no máximo 45 caracteres.',
-  email: 'Email deve ser válido e ter no máximo 80 caracteres.',
-  senhaHash: 'Senha deve ter no máximo 100 caracteres.',
-  foto: 'URL da foto deve ter no máximo 45 caracteres.',
-  apelido: 'Apelido deve ter no máximo 45 caracteres.',
-  nome: 'Nome deve ter no máximo 50 caracteres.',
-  cidade: 'Cidade deve ter no máximo 50 caracteres.',
-  sexo: 'Sexo deve ser "F", "M" ou "O".',
-  cpf: 'CPF deve ter no máximo 14 caracteres.',
-
-  descricao: 'Descrição deve ter no máximo 255 caracteres.',
-  nomePopular: 'Nome popular deve ter no máximo 45 caracteres.',
-  nomeCientifico: 'Nome científico deve ter no máximo 45 caracteres.',
-  som: 'Som deve ter no máximo 45 caracteres.',
-  medalha: 'Medalha deve ter no máximo 45 caracteres.',
-  estrela: 'Estrela deve ser um número máximo de 5.',
-  nascimento: 'Nascimento deve ser uma data válida.',
-
-  url: 'URL deve ter no máximo 45 caracteres.',
-  mostrarNoCarrosel: 'Mostrar no carrossel deve ser um número máximo de 255.',
-  dataDaVisita:
-    'Data da visita deve ser uma data válida com no máximo 45 caracteres.',
-  dataDaDescoberta:
-    'Data da descoberta deve ser uma data válida com no máximo 45 caracteres.',
-};
