e1a0409e342603cee2b64f95ecaa2f0ccbc82f95
leonardo <leonardolfelix12@gmail.com>
2024-05-05 00:58:03 -0300

    feat(prisma): criando middleware de softDelete

--- src/prisma.js
+++ src/prisma.js
@@ -2,5 +2,44 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default prisma;
- 
+/**
+ * @type {typeof prisma}
+ * */
+export default prisma.$extends({
+  query: {
+    $allModels: {
+      $allOperations({ args, operation, query, model }) {
+        if (operation === 'delete' || operation === 'deleteMany') {
+          const op = operation === 'deleteMany' ? 'updateMany' : 'delete';
+
+          return prisma[model][op]({
+            ...args,
+            data: {
+              deleted_at: new Date(),
+            },
+          });
+        }
+
+        deepChangeWhere(args, 'where', (e) => {
+          e.deleted_at = null;
+        });
+        return query(args);
+      },
+    },
+  },
+});
+
+function deepChangeWhere(arg, propsAlvo, mudanca) {
+  for (const key in arg) {
+    const element = arg[key];
+
+    if (key === propsAlvo) mudanca(element);
+
+    const isObject =
+      element !== null &&
+      typeof element === 'object' &&
+      !Array.isArray(element);
+
+    if (isObject) deepChangeWhere(element);
+  }
+}
