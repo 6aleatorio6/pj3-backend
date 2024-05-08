362ad159b54db0121f655f22307fb0495177adea
leonardo <leonardolfelix12@gmail.com>
2024-05-07 21:30:18 -0300

    fix(prisma): corrigido erro do softDelete  não estar passando pela extensao prismaController

--- src/services/customPrisma/softDelete.js
+++ src/services/customPrisma/softDelete.js
@@ -5,31 +5,29 @@ import prisma from '../../prisma.js';
  * */
 export const prismaSoftDelete = prisma.$extends({
   query: {
-    $allModels: {
-      $allOperations({ args, operation, query, model }) {
-        if (operation === 'delete' || operation === 'deleteMany') {
-          return prisma[model][operation.replace('delete', 'update')]({
-            ...args,
-            where: {
-              ...args.where,
-              deleted_at: null,
-            },
-            data: {
-              deleted_at: new Date(),
-            },
-          });
-        }
- 
-        deepChangeWhere(args, 'where', (e) => {
-          e.deleted_at = null;
+    $allModels({ query, model, operation, args }) {
+      if (operation === 'delete' || operation === 'deleteMany') {
+        return this[model][operation.replace('delete', 'update')]({
+          ...args,
+          where: {
+            ...args.where,
+            deleted_at: null,
+          },
+          data: {
+            deleted_at: new Date(),
+          },
         });
+      }
 
-        if (operation === 'findUnique' || operation === 'findUniqueOrThrow') {
-          return prisma[model][operation.replace('Unique', 'First')](args);
-        }
+      deepChangeWhere(args, 'where', (e) => {
+        e.deleted_at = null;
+      }); 
 
-        return query(args);
-      },
+      if (operation === 'findUnique' || operation === 'findUniqueOrThrow') {
+        return this[model][operation.replace('Unique', 'First')](args);
+      }
+
+      return query(args);
     },
   },
 });
