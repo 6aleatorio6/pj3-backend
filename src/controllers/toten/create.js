30f733ea9d9ecefd6b8e72f730f1fd9f159cd544
Thiago Inácio <thiagoil2006@gmail.com>
2024-05-27 15:30:53 -0300

    feat: ta pronto o post toten

--- src/controllers/toten/create.js
+++ src/controllers/toten/create.js
@@ -2,45 +2,37 @@ import createController from '../../helpers/createController.js';
 import { prismaPaiado } from '../../services/customPrisma/prismaController.js';
 import { reqValidy } from '../../services/validacao/reqValidy.js';
 
-export default createController( async (req, res) => {
+export default createController(async (req, res) => {
+  reqValidy(req, {
+    body: {
+      nome: 'required',
+      cidade: 'required',
+      sexo: 'required',
+      nascimento: 'required',
+    },
+  });
+  const usuarioParcial = await prismaPaiado.usuario.create({
+    select: {
+      id: true,
+      nome: true,
+      cidade: true,
+      sexo: true,
+      nascimento: true,
+      visitas: true,
+    },
+    data: {
+      ...req.body,
+      visitas: {
+        create: {
+          dataDaVisita: new Date(),
+        },
+      },
+    },
+  });
 
-    let dataAtual = new Date()
-
-    reqValidy(req, {
-        body: {
-            nome: 'required',
-            cidade: 'required',
-            sexo: 'required',
-            nascimento: 'required'
-        }
-    })
-
-    req.body.nascimento = dataAtual.setFullYear(dataAtual.getFullYear() - req.body.nascimento)
-
-    const usuarioParcial = await prismaPaiado.usuario.create({
-        select: {
-            id: true,
-            nome: true,
-            cidade: true,
-            sexo: true,
-            nascimento: true,
-        }, 
-        data: {
-            visitas: {
-            create: [
-                {
-                    dataDaVisita: new Date(),
-
-                }
-            ]
-        }},
-
-        data: req.body
-    })
-
-    const visita = await prismaPaiado.visita.create({
-        id: true
-    })
-
-    res.json({  message: `Visita ${visita} do usuário ${usuarioParcial} criado com sucesso!`, usuarioParcial, visita })
-})
+  res.json({
+    message: `Visita ${usuarioParcial.visitas.id} do usuário ${usuarioParcial.nome} criado com sucesso!`,
+    usuarioParcial,
+    visitas: usuarioParcial.visitas,
+  });
+});
--- src/routers/totenRouter.js
+++ src/routers/totenRouter.js
@@ -5,3 +5,5 @@ import create from '../controllers/toten/create.js';
 const router = Router()
 
 router.post('/visita', create)
+
+export default router;
