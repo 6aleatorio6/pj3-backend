956beeff7492dfa73e3921912c8241e396340468
leonardo <leonardolfelix12@gmail.com>
2024-06-01 01:07:19 -0300

    refactor(auth): logica jwt refeito sem passport

--- src/services/auth/guardJwt.js
+++ src/services/auth/guardJwt.js
@@ -1,8 +1,42 @@
+import jwt from 'jsonwebtoken';
 import createPaia from '../../helpers/createController.js';
+import { ErrorController } from '../../helpers/erroController.js';
+import { extractTokenFromHeader, jwtVerify } from './helpersAuth.js';
 
 /**
- * @param {("USER" | "ADM" | "TOTEM")[]} roles
+ * @param {"USER" | "ADM" | "TOTEM"} roles
  */
-export function useGuard(roles) {
-  return createPaia(async (req, res, next) => {});
+export function useGuard(...roles) {
+  return createPaia(async (req, res, next) => {
+    const token = extractTokenFromHeader(req);
+
+    if (!token) throw new ErrorController(401, 'você não está logado');
+
+    const payload = jwtLogica(token);
+
+    const isAuth = !roles.includes(payload.roles);
+
+    if (isAuth) throw new ErrorController(403, 'você não está autorizado');
+
+    req.user = {
+      id: payload.id,
+      roles: payload.roles,
+    };
+
+    next();
+  });
+}
+
+function jwtLogica(token) {
+  try {
+    return jwtVerify(token);
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError)
+      throw new ErrorController(401, 'token expirado');
+
+    if (error instanceof jwt.JsonWebTokenError)
+      throw new ErrorController(401, 'token invalido');
+
+    throw error;
+  }
 }
--- src/services/auth/helpersAuth.js
+++ src/services/auth/helpersAuth.js
@@ -1,12 +1,26 @@
 import { hashSync } from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+const saltOrRounds = Number(process.env.PASSWORD_SALTS) || 8;
+const secketJwt = process.env.SECKET || 'paia';
+const expiresJwt = process.env.JwtExpiresIn || '3m';
+
 export function gerarHash(senha) {
-  if (senha) return hashSync(senha, Number(process.env.PASSWORD_SALTS) || 8);
+  if (senha) return hashSync(senha, saltOrRounds);
 }
 
 export function jwtSign(payload) {
-  return jwt.sign(payload, process.env.SECKET, {
-    expiresIn: 1000 * 60 * process.env.JwtExpiresIn || 3,
+  return jwt.sign(payload, secketJwt, {
+    expiresIn: expiresJwt,
   });
+}
+
+export function jwtVerify(token) {
+  return jwt.verify(token, secketJwt);
+}
+
+export function extractTokenFromHeader(req) {
+  const [type, token] = req.headers.authorization?.split(' ') ?? [];
+
+  return type === 'Bearer' ? token : undefined;
 }
