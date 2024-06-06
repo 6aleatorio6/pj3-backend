a71e44ad41d82fece300ee38ae5888f223059e00
Bryan-Machado <bryan.machado.soares@gmail.com>
2024-06-06 15:50:43 -0300

    chore filtering date with moment

--- src/controllers/pdfPuppeteer/todosUsuariosPDF.js
+++ src/controllers/pdfPuppeteer/todosUsuariosPDF.js
@@ -1,6 +1,20 @@
 import relatorioModel from "../../services/pdfGenerate/relatorioModel.js";
 import generatePDF from "../../services/pdfGenerate/pdfGenerator.js";
 import generateHTML from "../../services/pdfGenerate/templates/generatePDFemHTML.js";
+import moment from "moment";
+
+const formatarData = (data) => {
+    // Tenta formatar a data em diferentes formatos aceitos
+    const formatos = ['YYYY-MM-DD', 'DD-MM-YYYY', 'YYYY/MM/DD', 'DD/MM/YYYY'];
+
+    for (const formato of formatos) {
+        const dataFormatada = moment(data, formato, true);
+        if (dataFormatada.isValid()) {
+            return dataFormatada.toISOString();
+        }
+    }
+    return null; // Caso nenhum formato seja válido
+};
 
 const todosUsuariosPDF = async (req, res) => {
     try {
@@ -15,11 +29,27 @@ const todosUsuariosPDF = async (req, res) => {
          const dataUmMesAFrenteFormatada = umMesAFrente.toISOString();
  
          if (!filtro.dataDaVisitaMin) {
-             filtro.dataDaVisitaMin = dataHojeFormatada;
-         }
-         if (!filtro.dataDaVisitaMax) {
-             filtro.dataDaVisitaMax = dataUmMesAFrenteFormatada;
-         }
+            filtro.dataDaVisitaMin = dataHojeFormatada;
+        } else {
+            const dataFormatada = formatarData(filtro.dataDaVisitaMin);
+            if (dataFormatada) {
+                filtro.dataDaVisitaMin = dataFormatada;
+            } else {
+                return res.status(400).json({ error: 'Formato de dataDaVisitaMin inválido' });
+            }
+        }
+
+        if (!filtro.dataDaVisitaMax) {
+            filtro.dataDaVisitaMax = dataUmMesAFrenteFormatada;
+        } else {
+            const dataFormatada = formatarData(filtro.dataDaVisitaMax);
+            if (dataFormatada) {
+                filtro.dataDaVisitaMax = dataFormatada;
+            } else {
+                return res.status(400).json({ error: 'Formato de dataDaVisitaMax inválido' });
+            }
+        }
+        
         const resultado = await relatorioModel.totalUsuarios(filtro)
         
         // const htmlContent = await compileTemplate(generateHTML, resultado) //antiga formatação html
