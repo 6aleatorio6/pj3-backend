b1a5b0b14bb980b3d5997d99324359deac755200
leonardo <leonardolfelix12@gmail.com>
2024-06-07 01:17:30 -0300

    feat(service/upload): criando serviço de upload de arquivos p#2

--- src/services/uploadFiles/upload.js
+++ src/services/uploadFiles/upload.js
@@ -1,54 +1,63 @@
+/* eslint-disable promise/param-names */
 import busboy from 'busboy';
 import endpointBoxSafe from '../secureController/handlerBox.js';
 import { uploadFile } from './pontasFiles.js';
 import { HttpException } from '../secureController/handlersPaia.js';
 
-export const convertFilesToURLs = endpointBoxSafe((req, res, next) => {
+export const convertFilesToURLs = endpointBoxSafe((req, res) => {
   return new Promise((resolve, reject) => {
     const isMultiPart =
       req.headers['content-type'] &&
       req.headers['content-type'].includes('multipart/form-data');
 
-    if (!isMultiPart) return next();
+    if (!isMultiPart) return resolve('nextPaia');
 
     const bb = busboy({
       headers: req.headers,
       limits: { fileSize: 10 * 1024 * 1024 },
     });
 
-    bb.on('field', (field, valor) => {
-      req.body[field] = valor;
+    bb.on('field', (field, value) => {
+      req.body[field] = value;
     });
 
-    bb.on('file', async (field, file, info) => {
-      const chunks = [];
-
-      file.on('data', (chunk) => chunks.push(chunk));
-
-      file.on('end', async () => {
-        try {
-          const fileInteiro = Buffer.concat(chunks);
-
-          const url = await uploadFile(fileInteiro, info);
-          req.body[field] = url;
-        } catch (e) {
-          console.log(e);
-
-          reject(e);
-        }
+    const paiaFiles = [];
+    bb.on('file', (field, file, info) => {
+      const arrayBuffer = [];
+      file.on('data', (c) => {
+        arrayBuffer.push(c);
       });
+
+      paiaFiles.push(
+        new Promise((res, rej) => {
+          file.on('end', async () => {
+            try {
+              const uriFile = await uploadFile(Buffer.from(arrayBuffer), info);
+              req.body[field] = uriFile;
+              res(uriFile);
+            } catch (error) {
+              rej(error);
+            }
+          });
+        }),
+      );
     });
 
     bb.on('finish', () => {
-      next();
-      console.log(req.body);
-      resolve();
+      Promise.all(paiaFiles)
+        .then(() => resolve('nextPaia'))
+        .catch(reject);
     });
 
     bb.on('error', (e) => {
-      console.log(e);
-
-      reject(new HttpException(500, 'dsf', e));
+      if (e instanceof HttpException) return reject(e);
+      reject(
+        new HttpException(
+          500,
+          'erro ao processar o formulario MultiPart',
+          e.message,
+        ),
+      );
     });
 
     req.pipe(bb);
