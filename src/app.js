543f8ca5f5432623145a662d0bee8ec6a1322175
leonardo <leonardolfelix12@gmail.com>
2024-06-07 23:31:39 -0300

    fix: pequenas correções

--- src/app.js
+++ src/app.js
@@ -19,11 +19,11 @@ export const corsOptions = JSON.parse(
 
 // middleware
 app.use(
-  loggerMiddleware,
   cors(corsOptions),
   express.json(),
   express.urlencoded({ extended: false }),
   convertFilesToURLs, // mais pra frente tem que mover para dps da autenticacao
+  loggerMiddleware,
   cookieParser(),
 );
 
--- src/helpers/loggerMidleware.js
+++ src/helpers/loggerMidleware.js
@@ -1,17 +1,31 @@
+function formatBytes(bytes) {
+  const sizes = ['bytes', 'KB', 'MB'];
+  if (bytes === 0) return '0 bytes';
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+  return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
+}
+
 export function loggerMiddleware(req, res, next) {
   const startTime = Date.now();
   const { method, url, headers } = req;
   const userAgent = headers['user-agent'].split(' ').slice(-3, -1).join(' ');
 
+  const requestContentLength = req.headers['content-length'];
+  const requestContentLengthStr = requestContentLength
+    ? formatBytes(parseInt(requestContentLength, 10))
+    : 'N/A';
+
   res.on('finish', () => {
     const endTime = Date.now();
     const responseTime = endTime - startTime;
     const statusCode = res.statusCode;
-    const contentLength = res.getHeader('content-length');
-    const contentLengthStr = contentLength ? `${contentLength} bytes` : 'N/A';
+    const responseContentLength = res.getHeader('content-length');
+    const responseContentLengthStr = responseContentLength
+      ? formatBytes(parseInt(responseContentLength, 10))
+      : 'N/A';
 
     console.log(
-      `${method} ${url} - ${statusCode} - ${contentLengthStr} - ${responseTime}ms - ${userAgent}`,
+      `${method} ${url} - ${statusCode} - Req: ${requestContentLengthStr} - Res: ${responseContentLengthStr} - ${responseTime}ms - ${userAgent}`,
     );
   });
 
