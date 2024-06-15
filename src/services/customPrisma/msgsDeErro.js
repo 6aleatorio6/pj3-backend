379b135f62f3ae6f98f929f8c2f943abad3ef473
leonardo <leonardolfelix12@gmail.com>
2024-06-15 01:32:28 -0300

    feat(service/customPrisma): melhoria no tratamento de erros do Prisma

--- src/services/customPrisma/msgsDeErro.js
+++ src/services/customPrisma/msgsDeErro.js
@@ -1,54 +1,73 @@
 /* eslint-disable camelcase */
 
+import { HttpException } from '../secureController/handlersPaia.js';
+
 /**
  *
  * @param {import('@prisma/client/runtime/library').PrismaClientKnownRequestError} e
+ * @param {string} modelOriginal
  */
-export const errosSabidos = (e, modelOrignal) => {
-  const { modelName: modelError, target, column_name } = e.meta || {};
-
-  const model = modelError || modelOrignal;
-
-  const col = column_name || target?.split('_')[1] || model;
+export const errosSabidos = (e, modelOriginal) => {
+  const { code, message, meta } = e;
 
   const errosPeloCod = {
-    P2000: [400, `Valor muito longo para o campo ${col}.`],
-    P2001: [404, `${model} não encontrado.`],
-    P2002: [400, `esse ${col} já está sendo usado.`],
-    P2003: [400, `Chave estrangeira inválida.`],
-    P2004: [400, `Erro de restrição no banco.`],
-    P2005: [400, `Valor inválido.`],
-    P2006: [400, `Valor inválido.`],
-    P2007: [400, `Erro de validação.`],
-    P2008: [400, `Erro na consulta.`],
-    P2009: [400, `Erro de validação na consulta.`],
-    P2010: [400, `Erro na consulta.`],
-    P2011: [400, `Valor não pode ser nulo.`],
-    P2012: [400, `Falta um valor obrigatório.`],
-    P2013: [400, `Falta um argumento obrigatório.`],
-    P2014: [400, `Relação necessária violada.`],
-    P2015: [400, `${model} não encontrado.`],
-    P2016: [400, `Erro na consulta.`],
-    P2017: [400, `${model}s não conectados.`],
-    P2018: [400, `Falta conexão entre ${model}s.`],
-    P2019: [400, `Erro de entrada.`],
-    P2020: [400, `Valor fora do intervalo.`, e?.meta?.details],
-    P2021: [404, `Tabela não encontrada.`],
-    P2022: [404, `Coluna não encontrada.`],
-    P2023: [400, `Dados inconsistentes em ${col}.`],
-    P2024: [503, `Tempo esgotado para nova conexão.`],
-    P2025: [400, `${model} não encontrado.`],
-    P2026: [400, `Recurso não suportado.`],
-    P2027: [400, `Vários erros na consulta.`],
-    P2028: [400, `Erro na transação.`],
-    P2029: [400, `Limite de parâmetros excedido.`],
-    P2030: [400, `Índice não encontrado.`],
-    P2033: [400, `Número muito grande.`],
-    P2034: [400, `Falha na transação.`],
-    P2035: [400, `Violação de asserção.`],
-    P2036: [400, `Erro no conector.`],
-    P2037: [400, `Muitas conexões abertas.`],
+    P2000: [
+      400,
+      `O valor fornecido para o campo '${meta?.target}' é muito longo.`,
+      meta,
+    ],
+    P2001: [404, `${meta?.target || modelOriginal} não encontrado.`, meta],
+    P2002: [400, `O campo '${meta?.target}' já está em uso.`, meta],
+    P2003: [400, `Chave estrangeira inválida: ${message}`, meta],
+    P2004: [400, `Erro de restrição no banco de dados: ${message}`, meta],
+    P2005: [400, `Valor inválido: ${message}`, meta],
+    P2006: [400, `Valor inválido: ${message}`, meta],
+    P2007: [400, `Erro de validação: ${message}`, meta],
+    P2008: [400, `Erro na consulta: ${message}`, meta],
+    P2009: [400, `Erro de validação na consulta: ${message}`, meta],
+    P2010: [400, `Erro na execução da consulta: ${message}`, meta],
+    P2011: [400, `Valor não pode ser nulo para '${meta?.target}'.`, meta],
+    P2012: [400, `Falta um valor obrigatório para '${meta?.target}'.`, meta],
+    P2013: [400, `Falta um argumento obrigatório: ${message}`, meta],
+    P2014: [400, `Violação de integridade referencial: ${message}`, meta],
+    P2015: [400, `${meta?.target || modelOriginal} não encontrado.`, meta],
+    P2016: [400, `Erro na consulta: ${message}`, meta],
+    P2017: [
+      400,
+      `Registros relacionados não encontrados para '${meta?.target || modelOriginal}'.`,
+      meta,
+    ],
+    P2018: [
+      400,
+      `Falta conexão entre ${meta?.target || modelOriginal}(s) especificados.`,
+      meta,
+    ],
+    P2019: [400, `Erro de entrada: ${message}`, meta],
+    P2020: [
+      400,
+      `Valor fora do intervalo permitido para '${meta?.target}'.`,
+      meta,
+    ],
+    P2021: [404, `Tabela não encontrada.`, meta],
+    P2022: [404, `Coluna não encontrada.`, meta],
+    P2023: [
+      400,
+      `Dados inconsistentes em '${meta?.target || modelOriginal}'.`,
+      meta,
+    ],
+    P2024: [503, `Tempo esgotado para nova conexão.`, meta],
+    P2025: [400, `${meta?.target || modelOriginal} não encontrado.`, meta],
+    P2026: [400, `Recurso não suportado: ${message}`, meta],
+    P2027: [400, `Vários erros na consulta: ${message}`, meta],
+    P2028: [400, `Erro na transação: ${message}`, meta],
+    P2029: [400, `Limite de parâmetros excedido: ${message}`, meta],
+    P2030: [400, `Índice não encontrado: ${message}`, meta],
+    P2033: [400, `Número muito grande: ${message}`, meta],
+    P2034: [400, `Falha na transação: ${message}`, meta],
+    P2035: [400, `Violação de asserção: ${message}`, meta],
+    P2036: [400, `Erro no conector: ${message}`, meta],
+    P2037: [400, `Muitas conexões abertas: ${message}`, meta],
   };
 
-  return errosPeloCod[e.code] || [400, e.message];
+  if (errosPeloCod[code]) throw new HttpException(...errosPeloCod[code]);
 };
--- src/services/customPrisma/prismaController.js
+++ src/services/customPrisma/prismaController.js
@@ -1,6 +1,12 @@
 import { Prisma } from '@prisma/client';
 import { HttpException } from '../secureController/handlersPaia.js';
 import { errosSabidos } from './msgsDeErro.js';
+import {
+  PrismaClientInitializationError,
+  PrismaClientRustPanicError,
+  PrismaClientUnknownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
 
 export const PrismaErrorInterceptor = Prisma.defineExtension((dbClient) =>
   dbClient.$extends({
@@ -10,31 +16,44 @@ export const PrismaErrorInterceptor = Prisma.defineExtension((dbClient) =>
           return await query(args);
         } catch (e) {
           if (e instanceof HttpException) throw e;
-          console.log(e.meta || e.message);
-          const errosTrataveis = [
-            [Prisma.PrismaClientKnownRequestError, errosSabidos(e, model)],
-            [
-              Prisma.PrismaClientInitializationError,
-              [500, 'Erro ao se comunicar com o banco de dados'],
-            ],
-            [
-              Prisma.PrismaClientValidationError,
-              [
-                500,
-                'Chamada do Prisma inválida. Verifique a sua consulta do prisma (se for dev)',
-                e.stack,
-              ],
-            ],
-          ];
-          const achandoErro = errosTrataveis.find(([er]) => e instanceof er);
 
-          if (!(e instanceof Prisma.PrismaClientKnownRequestError)) {
-            // console.error(e);
+          if (e?.code) errosSabidos(e, model); // se for um erro sabido dara erro
+
+          // Log do erro para depuração
+          console.error('Erro do Prisma:', e);
+
+          // Tratamento específico para diferentes tipos de erros do Prisma
+          const errorHandlers = {
+            [PrismaClientInitializationError.name]: [
+              500,
+              'Erro de inicialização do Prisma. Verifique a conexão com o banco de dados.',
+            ],
+            [PrismaClientValidationError.name]: [
+              400,
+              'Erro de validação do Prisma. Verifique os dados enviados.',
+              e.stack,
+            ],
+            [PrismaClientRustPanicError.name]: [
+              500,
+              'Erro interno do Prisma. Consulte os logs para mais detalhes.',
+            ],
+            [PrismaClientUnknownRequestError.name]: [
+              500,
+              'Erro desconhecido do Prisma. Consulte os logs para mais detalhes.',
+            ],
+          };
+
+          // Executa o handler correspondente se o tipo de erro for reconhecido
+          const handler = Object.keys(errorHandlers).find(
+            (key) => key === e.name,
+          );
+
+          if (handler) {
+            throw new HttpException(...errorHandlers[handler]);
           }
-          if (!achandoErro)
-            throw new HttpException(500, 'Erro desconhecido do Prisma');
 
-          throw new HttpException(...achandoErro[1]);
+          // Se nenhum handler correspondente for encontrado, lança um erro genérico
+          throw new HttpException(500, 'Erro desconhecido', e.message);
         }
       },
     },
