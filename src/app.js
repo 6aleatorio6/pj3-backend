7d9241f77ee3dd99671d420fea92fecc529e8600
leonardo <leonardolfelix12@gmail.com>
2024-06-15 23:43:26 -0300

    refactor:  rotas em arquivo separado e organizando arquivo principal (`app.js`)

--- src/app.js
+++ src/app.js
@@ -1,30 +1,21 @@
-import cookieParser from 'cookie-parser';
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import rotas from './index.js';
+import { origin } from './helpers/corsOrigin.js';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
-
-import funcionarioRouter from './routers/funcionarioRouter.js';
-import usuarioRouter from './routers/usuarioRouter.js';
-import catalogoRouter from './routers/catalogoRouter.js';
-import pdfRouter from './routers/pdfRouter.js';
-import excelRouter from './routers/excelRouter.js';
-import totenRouter from './routers/totenRouter.js';
 import { loggerMiddleware } from './helpers/loggerMidleware.js';
-import refreshSession from './controllers/auth/refreshSession.js';
 import { convertFilesToURLs } from './services/uploadFiles/upload.js';
-import { getFilesEndpoint } from './services/uploadFiles/pontasFiles.js';
+import { baseUrl } from './helpers/getBaseUrl.js';
 
 const app = express();
 
 app.get('/', (req, res) => res.json({ message: 'servidor online!' }));
 
-// cors
-export const corsOptions = JSON.parse(
-  `{"origin": ${process.env.CORS_ORIGIN || '"*"'} }`,
-);
-
-// middleware
 app.use(
-  cors(corsOptions),
+  baseUrl.capturar,
+  cors({ origin }),
   express.json(),
   express.urlencoded({ extended: false }),
   convertFilesToURLs, // mais pra frente tem que mover para dps da autenticacao
@@ -32,30 +23,16 @@ app.use(
   cookieParser(),
 );
 
-// services
-app.get('/files/:storage/?', getFilesEndpoint);
-app.use('/token/refresh', refreshSession);
+app.use(rotas);
 
-// controllers
-app.use('/funcionario', funcionarioRouter);
-app.use('/usuario', usuarioRouter);
-app.use('/catalogo', catalogoRouter);
-app.use('/geraPdf', pdfRouter);
-app.use('/toten', totenRouter);
-app.use('/excel', excelRouter);
+const serverApp = createServer(app);
 
-// ERROS
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'esse endpoint não existe',
-  });
-});
-app.use((error, req, res, next) => {
-  console.error(error.stack);
-  res.status(500).json({
-    message: 'error handler',
-    error,
+const io = new Server(serverApp, { cors: { origin } });
+
+io.on('connection', (socket) => {
+  socket.on('connect', () => {
+    console.log('hello paia');
   });
 });
 
-export default app;
+export default serverApp;
