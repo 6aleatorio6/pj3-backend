04e9431ebd34d69fdb4bc1434f04371d1be49e33
leonardo <leonardolfelix12@gmail.com>
2024-06-16 15:39:39 -0300

    feat(user/getbytoken): add catalogoNLido a resposta do get usuario

--- src/controllers/usuario/getByToken.js
+++ src/controllers/usuario/getByToken.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import endpointBoxSafe from '../../services/secureController/handlerBox.js';
 import { prismaPaiado } from '../../prisma.js';
 
@@ -12,8 +13,6 @@ import { prismaPaiado } from '../../prisma.js';
  *      APP MOBILE
  */
 export default endpointBoxSafe(async (req, res) => {
-  const id = +req.user.id;
-
   const usuario = await prismaPaiado.usuario.findFirstOrThrow({
     select: {
       id: true,
@@ -21,12 +20,37 @@ export default endpointBoxSafe(async (req, res) => {
       foto: true,
       lidoPeloUser: {
         distinct: ['catalogo_uuid'],
-        select: { dataDaDescoberta: true, catalogo: true },
+        select: {
+          dataDaDescoberta: true,
+          catalogo_uuid: true,
+        },
         orderBy: { dataDaDescoberta: 'asc' },
       },
     },
-    where: { id },
+    where: { id: req.user.id },
   });
+
+  const catalogo = await prismaPaiado.catalogo.findMany({
+    select: {
+      uuid: true,
+      nomePopular: true,
+      nomeCientifico: true,
+      especie: true,
+      ftModel: true,
+      medalha: true,
+    },
+  });
+
+  usuario.catalogoNLido = catalogo.filter(
+    ({ uuid }) =>
+      !usuario.lidoPeloUser.some(({ catalogo_uuid }) => uuid === catalogo_uuid),
+  );
+
+  usuario.lidoPeloUser = usuario.lidoPeloUser.map((lido) => ({
+    ...lido,
+    catalogo: catalogo.find((item) => item.uuid === lido.catalogo_uuid),
+    catalogo_uuid: undefined,
+  }));
 
   usuario.progresso = {
     total: await prismaPaiado.catalogo.count(),
