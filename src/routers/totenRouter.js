de7c2ef9bfa534e2b6d3946d478c155b7955cf4f
leonardo <leonardolfelix12@gmail.com>
2024-06-15 22:13:53 -0300

    fix(auth): rotas pdf e toten protegidas;  jsDoc do useGuard corrigido

--- src/routers/totenRouter.js
+++ src/routers/totenRouter.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 
 import create from '../controllers/toten/create.js';
-import getToten from '../controllers/toten/getToten.js';
+import { useGuard } from '../services/auth/guardJwt.js';
 
 const router = Router();
 
-router.post('/visita', create);
-router.get('/visita/:dataToten', getToten);
+router.post('/visita', useGuard('TOTEM'), create);
 
 export default router;
