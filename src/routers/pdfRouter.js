1037a32d3b1d151c969f4a9c6569723d2a2bee95
ARTHURcantu <cantuario.arthur@gmail.com>
2024-06-17 18:02:26 -0300

    add guard  to pdf router

--- src/routers/pdfRouter.js
+++ src/routers/pdfRouter.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import todasVisitasPDF from '../controllers/pdfPuppeteer/todasVisitasPDF.js';
+import { useGuard } from '../services/auth/guardJwt.js';
 
 const router = Router();
 
 
-router.get('/listar-visitas', todasVisitasPDF)
+router.get('/listar-visitas', useGuard('ADM'), todasVisitasPDF)
 
 
 
